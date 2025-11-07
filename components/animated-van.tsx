9b5d0a93b6ab946fecc7064823e9852002523a80
cristian.baltaga <cristian.baltaga@ballys.com>
2025-11-07 16:56:12 +0000

    Remove basePath for custom domain

--- components/animated-van.tsx
+++ components/animated-van.tsx
@@ -6,7 +6,7 @@ export function AnimatedVan() {
       {/* Van body */}
       <div className="animate-van-move">
         <img
-          src="/velamoving/moving-van.png"
+          src="/moving-van.png"
           alt="Moving van"
           className="w-[500px] h-auto drop-shadow-2xl"
         />
